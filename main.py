18ddfffb44eddcb49758aaf6f407fc98e726f925
Daniel Rocher <erable@adella.org>
2017-03-29 11:09:20 +0200

    Ajout methode Main

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import time, sys
+
 def removeCRLF(msg):
     return msg.replace('\n','').replace('\r', '')
 
+def Main(configfilename):
+    sys.exit(0)
+
+if __name__ == "__main__":
+    Main("/etc/projet1.conf")
 
