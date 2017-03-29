31fb723fdeb1084c08d66898d3dc67e752b554bc
Daniel Rocher <erable@adella.org>
2017-03-29 13:49:47 +0200

    gestion des arguments

--- main.py
+++ main.py
@@ -2,12 +2,17 @@
 # -*- coding: utf-8 -*-
 
 import time, sys
-import configparser
+import argparse,configparser
 
 def removeCRLF(msg):
     return ''.join(msg.splitlines())
 
 def Main(configfilename):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--version", help="show version", action="store_true")
+    parser.add_argument("--debug", help="Debug", action="store_true")
+    args=parser.parse_args()
+
     config = configparser.ConfigParser()
     config.read(configfilename)
     try:
