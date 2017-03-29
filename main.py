ac94391453a6dfafa12847fcfcfcc6c67ec96c24
Daniel Rocher <erable@adella.org>
2017-03-29 11:15:21 +0200

    Lecture du fichier de configuration

--- main.py
+++ main.py
@@ -2,12 +2,24 @@
 # -*- coding: utf-8 -*-
 
 import time, sys
+import configparser
 
 def removeCRLF(msg):
     return msg.replace('\n','').replace('\r', '')
 
 def Main(configfilename):
+    config = configparser.ConfigParser()
+    config.read(configfilename)
+    try:
+        # section DEFAULT
+        num=config["GLOBAL"]["num"]
+        whitelist=["GLOBAL"]["whitelist"]
+        blacklist=["GLOBAL"]["blacklist"]
+    except:
+        print ("Unable to retrieve configuration data in {}:\n{}".format(configfilename, sys.exc_info()[0]))
+        sys.exit(1)
     sys.exit(0)
+
 
 if __name__ == "__main__":
     Main("/etc/projet1.conf")
